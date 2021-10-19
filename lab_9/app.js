7a4875352652b177bf916123a8823d3c46f80516
Vasiliy <wudgexyz@gmail.com>
2021-10-19 18:14:24 +0300

    Lab 9: Task 2 fix

--- lab_9/app.js
+++ lab_9/app.js
@@ -11,6 +11,17 @@ function getHashTags(text) {
 }
 console.log(getHashTags("Прохожу курс по #javascript и #web и ещё #много #хештегов #мрр #cat_id_rsa"));
 console.log("# Task 2");
-function normalizeHashTags(text) {
-
+function normalizeHashTags(hashtags) {
+    /*
+     reduce Функция, выполняющаяся для каждого элемента массива, принимает четыре аргумента:
+     аккумулятор - накапливает значение от калбека
+    */
+    return hashtags.reduce((accum, tag) => {
+        if (!accum.includes(tag.toLowerCase())) { // если массив не содержит тег (приведенный к маленьким буквам)
+            accum.push(tag.toLowerCase()); // добавляет тег маленькими буквами
+        }
+        return accum;
+    }, []);
 }
+
+console.log(normalizeHashTags(["Web", "hashtag", "web", "JavaScript", "Script", "coding"]));
