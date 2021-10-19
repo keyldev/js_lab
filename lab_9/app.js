b421b6df65a3d6db65e404525333bed96768da80
Vasiliy <wudgexyz@gmail.com>
2021-10-19 17:09:54 +0300

    Lab 9: Task 1 fix

--- lab_9/app.js
+++ lab_9/app.js
@@ -1,12 +1,10 @@
 console.log("# Task 1")
 
 function getHashTags(text) {
-    
-    let newTextArray = [];
-    let textToArray = text.split(" ");
-    for (var i = 0; i < textToArray.length; i++) {
-        var subString = textToArray[i];
 
+    const newTextArray = [];
+    const textToArray = text.split(" ");
+    for(let subString of textToArray){
         if(subString[0] == "#"){
             newTextArray.push(subString.slice(1));
         }
@@ -14,3 +12,7 @@ function getHashTags(text) {
     return newTextArray;
 }
 console.log(getHashTags("Прохожу курс по #javascript и #web и ещё #много #хештегов #мрр #cat_id_rsa"));
+console.log("# Task 2");
+function normalizeHashTags(text) {
+    
+}
