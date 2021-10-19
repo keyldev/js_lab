99508c35f338a4731e327433bfff2dbfc844cf5f
Vasiliy <wudgexyz@gmail.com>
2021-10-19 14:42:12 +0300

    Task 1 ready

---
+++ lab_8/app.js
@@ -0,0 +1,10 @@
+console.log("# Task 1");
+
+function sum(a, b){
+    return Number(a) + Number(b);
+}
+console.log("Сумма 2 инта: " + sum(1,5));
+console.log("Сумма инт чар: " + sum(5, + '1'));
+console.log("Сумма строк: " + sum("12 а", "11 а"));
+
+console.log("#_#_#_#_#")
