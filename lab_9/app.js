cef55c0af363ca02960ef89493110f91ff47a740
Vasiliy <wudgexyz@gmail.com>
2021-10-19 20:41:59 +0300

    lab 9 task 3 fix

--- lab_9/app.js
+++ lab_9/app.js
@@ -9,7 +9,7 @@ function getHashTags(text) {
     }
     return newTextArray;
 }
-console.log(getHashTags("Прохожу курс по #javascript и #web и ещё #много #хештегов #мрр #cat_id_rsa"));
+// console.log(getHashTags("Прохожу курс по #javascript и #web и ещё #много #хештегов #мрр #cat_id_rsa"));
 console.log("# Task 2");
 function normalizeHashTags(hashtags) {
     /*
@@ -24,4 +24,49 @@ function normalizeHashTags(hashtags) {
     }, []);
 }
 
-console.log(normalizeHashTags(["Web", "hashtag", "web", "JavaScript", "Script", "coding"]));
+// console.log(normalizeHashTags(["Web", "hashtag", "web", "JavaScript", "Script", "coding"]));
+console.log("# Task 3");
+
+var phone = {};
+
+function startTask(command) {
+
+    if (command.includes("ADD")) {
+        var line = command.slice(3).trim();
+        var name = line.slice(0, line.indexOf(" "));
+        line = line.slice(line.indexOf(" ")).trim();
+        let numbers = line.split(",");
+
+        if (name in phone) {
+            phone[name] = phone[name].concat(numbers);
+        }
+        else {
+            phone[name] = numbers;
+        }
+    }
+    else if (command.includes("SHOW")) {
+        var showPhones = [];
+        for (const name in phone) {
+            showPhones.push(name + ": " + phone[name].join(", "));
+        }
+        return showPhones.sort();
+    }
+    else if (command.includes("REMOVE_PHONE")) {
+        let number = command.slice(13);
+        for (const i in phone) {
+            if (phone[i].includes(number)) {
+                phone[i].splice(phone[i].indexOf(number), 1);
+                if (phone[i].length == 0)
+                    delete phone[i];
+                return true;
+            }
+        }
+        return false;
+    }
+}
+startTask("ADD Name1 555-888-555-888");
+startTask("ADD Name1 555-888-555-82");
+startTask("ADD Name2 555-888-555-888");
+console.log(startTask("SHOW"));
+startTask("REMOVE_PHONE 555-888-555-888");
+console.log(startTask("SHOW"));
